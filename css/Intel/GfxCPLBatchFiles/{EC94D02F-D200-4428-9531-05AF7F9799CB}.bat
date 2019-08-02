efb430eded99e1401d061dbc99f07df4c0af049e
hudahussein <h.hussein.83@gmail.com>
2019-08-02 13:35:54 +0200

    new commit

---
+++ css/Intel/GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\k127153.inf_amd64_3f3936d8dec668b8\igfxEM.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\k127153.inf_amd64_3f3936d8dec668b8\igfxEM.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
